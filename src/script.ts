935b2941580afb942c7c808d38467283a91d2215
GrygorenkoMykhailo <osnova2021@gmail.com>
2024-07-12 20:16:16 +0300

    init ts

---
+++ src/script.ts
@@ -0,0 +1,34 @@
+
+class Game{
+    //button
+    //levels
+    //baseUpgrades
+}
+
+console.log(2)
+
+class Level{
+    // constructor(backgroundPath: string, upgrades: Upgrade[]){
+    //     this.backgroundPath = backgroundPath;
+    //     this.upgrades = upgrades;
+    // }
+}
+
+abstract class BaseUpgrade{
+    
+}
+
+interface IUpgrade{
+
+}
+
+type UpgradeType = {
+
+}
+
+// class Upgrade{
+//     constructor(name, backgroundPath, price, bonus){
+        
+//     }
+// }
+
